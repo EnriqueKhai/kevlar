b9c8f35f7c2de2b1caae8888fa17c9b1b7d0cd88
Nigelotan00 <nigelotan00@gmail.com>
2020-12-15 20:52:06 +0800

    Updated source_code.cpp with a new template.

--- settings/templates/source_code.cpp
+++ settings/templates/source_code.cpp
@@ -1,9 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Literals.
+#define nl                   "\n"
+
+// Primitives and Abstracts.
+#define ll                   long long
+#define vll                  vector<long long>
+#define vpll                 vector<pair<long long, long long>>
+#define vvll                 vector<vector<long long>>
+#define vvpll                vector<vector<pair<long long, long long>>>
+
+// Functions.
+#define read(x)              (cin >> x, x)
+#define forn(i,n)            for (long long i = 0; i < n; i += 1)
+#define rforn(i,n)           for (long long i = n - 1; i >= 0; i -= 1)
+#define lower(ch)            ('a'<=ch&&ch<='z')
+#define upper(ch)            ('A'<=ch&&ch<='Z')
+#define digit(ch)            ('0'<=ch&&ch<='9')
+#define in(x,y,z)            (x<=y&&y<=z)
+
+// Semantics.
+#define pre_if               if
+#define post_if              else
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    
+
     return 0;
 }
